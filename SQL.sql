2b5c9a4ad44f2fce82592c3d02e8b32716dfe18a
daisssukeeee <148407397+daisssukeeee@users.noreply.github.com>
2024-01-07 20:38:00 +0900

    Create SQL.sql

---
+++ SQL.sql
@@ -0,0 +1,16 @@
+INSERT INTO hospital(name,address,tel,opentime,closed,homepage,comment)
+VALUES('むらかみ内科クリニック','熊本県熊本市東区山ノ神2-2-32','096-331-2551','月〜金8:30〜12:00、14:30〜18:00
+土8:30〜12:00','日、祝','http://www.murakaminaika096.com/','体のこと、心のこと何でもご相談下さい。
+ご高齢の方で、近隣の患者様への往診を行っております。ご希望の方は、お気軽にご相談ください。');
+
+INSERT INTO hospital(name,address,tel,opentime,closed,homepage,comment)
+VALUES('有明メンタルクリニック','熊本県荒尾市増永2800-6','0968-69-1780','月〜金10:00〜14:00、15:00〜19:00','第2木、土、日、祝','http://www.ariakementalclinic.jp/','児童思春期の問題、うつ病などのメンタルヘルス、アルコール、薬物、ギャンブルなどの依存症問題、DVやストーカーをはじめとした暴力問題、パーソナリティ(性格)や行動上の問題など、幅広い「悩み」のご相談をお受けいたします。');
+
+INSERT INTO hospital(name,address,tel,opentime,closed,homepage,comment)
+VALUES('熊本駅前木もれびの森心療内科精神科','熊本県熊本市西区春日2丁目1-24
+グッドライフ熊本駅前 2F','096-354-8720','月〜水9:30〜12:00
+金〜土9:30〜12:00','木、日、祝','https://www.kumamoto-mentalclinic.com/','医療理念「優しい医療で、みんなに笑顔を」
+優しい医療とは・・・
+・副作用の少ない優しい医療を目指します。
+・薬剤より栄養状態を重視した優しい医療を目指します。
+・わかりやすい説明による優しい医療を目指します。');
